b496ad214290ca53d9ee58353acc56fa910e841a
alok0x0 <71223420+alok0x0@users.noreply.github.com>
2021-08-08 13:53:57 +0530

    ok

--- kodeo/settings.py
+++ kodeo/settings.py
@@ -14,7 +14,7 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+#BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
@@ -121,12 +121,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles')
 #STATICFILES_DIRS = [
 #    BASE_DIR / "static",
 #    '/var/www/static/',
 #]
-STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles')
+
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
