a98cf33571857782eae516030745b1833d74c913
alok0x0 <71223420+alok0x0@users.noreply.github.com>
2021-08-08 13:28:45 +0530

    new

--- kodeo/settings.py
+++ kodeo/settings.py
@@ -125,7 +125,7 @@ STATIC_URL = '/static/'
 #    BASE_DIR / "static",
 #    '/var/www/static/',
 #]
-STATIC_ROOT=os.join(BASE_DIR,'staticfiles')
+STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
