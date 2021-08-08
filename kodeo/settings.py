ca4f6e487b652db79d0dfdfb937a04fbeabc57c6
alok0x0 <71223420+alok0x0@users.noreply.github.com>
2021-08-08 14:31:20 +0530

    join

--- kodeo/settings.py
+++ kodeo/settings.py
@@ -15,6 +15,8 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 #BASE_DIR = Path(__file__).resolve().parent.parent
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+TEMPLATE_DIR = os.path.join(BASE_DIR,'templates')
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
@@ -38,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'kst'
+    'appTwo'
 ]
 
 MIDDLEWARE = [
@@ -59,6 +62,8 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': ['Templates'],
+        'DIRS': [TEMPLATE_DIR,],
+
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
